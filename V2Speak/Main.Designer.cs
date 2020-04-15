38d85f73afb6a0b44ef08e21423e55a2044eed7b
mya0 <mya0@protonmail.ch>
2020-04-15 19:06:01 +0900

    create v2s

---
+++ V2Speak/Main.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace V2Speak {
+    partial class Main {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.speakContent = new System.Windows.Forms.TextBox();
+            this.speakButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // speakContent
+            // 
+            this.speakContent.AcceptsReturn = true;
+            this.speakContent.Location = new System.Drawing.Point(12, 12);
+            this.speakContent.Multiline = true;
+            this.speakContent.Name = "speakContent";
+            this.speakContent.Size = new System.Drawing.Size(776, 397);
+            this.speakContent.TabIndex = 0;
+            // 
+            // speakButton
+            // 
+            this.speakButton.Location = new System.Drawing.Point(12, 415);
+            this.speakButton.Name = "speakButton";
+            this.speakButton.Size = new System.Drawing.Size(776, 23);
+            this.speakButton.TabIndex = 1;
+            this.speakButton.Text = "Speak";
+            this.speakButton.UseVisualStyleBackColor = true;
+            this.speakButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.speakButton);
+            this.Controls.Add(this.speakContent);
+            this.Name = "Main";
+            this.Text = "V2Speak";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox speakContent;
+        private System.Windows.Forms.Button speakButton;
+    }
+}
+
---
+++ V2Speak/Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using System.Windows.Forms;
+
+namespace V2Speak {
+    public partial class Main : Form {
+        private readonly string[] voiceroid2Titles = new string[] { "VOICEROID2", "VOICEROID2*" };
+
+        private ValuePattern v2Text;
+        private InvokePattern v2PlayButton;
+        public Main() {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e) {
+            AutomationElementCollection elements = AutomationElement.RootElement.FindAll(TreeScope.Children, Condition.TrueCondition);
+            AutomationElement v2element = null ;
+            foreach (AutomationElement element in elements) {
+                if(voiceroid2Titles.Contains(element.Current.Name)) {
+                    v2element = element;
+                    break;
+                }
+            }
+            if(v2element == null) {
+                MessageBox.Show("VOICEROID2が起動していません");
+                Application.Exit();
+            }
+            v2Text = v2element.FindAll(TreeScope.Descendants, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit))[0].GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
+            AutomationElementCollection v2Elements = v2element.FindAll(TreeScope.Descendants, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Button));
+            foreach(AutomationElement element in v2Elements) {
+                AutomationElement playtext = element.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Text));
+                if(playtext != null && playtext.Current.Name == "再生") {
+                    v2PlayButton = element.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+                    break;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            v2Text.SetValue(speakContent.Text);
+            v2PlayButton.Invoke();
+            Activate();
+        }
+    }
+}
